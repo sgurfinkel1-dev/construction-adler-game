a9f746eb8d20f1a14400550a8e383441b6aa9feb
Saul Godoi <sgurfinkel1@gmail.com>
2026-06-20 12:16:26 -0300

    Add package-lock.json and .gitignore for vercel deployment

--- node_modules/.pnpm/magicui@https+++codeload.gi_04c1659a44c5d75b95184ae0a952361a/node_modules/magicui/apps/www/registry/example/animated-theme-toggler-triangle-demo.tsx
+++
@@ -1,9 +0,0 @@
-import { AnimatedThemeToggler } from "@/registry/magicui/animated-theme-toggler"
-
-export default function AnimatedThemeTogglerTriangleDemo() {
-  return (
-    <div className="flex justify-center p-6">
-      <AnimatedThemeToggler variant="triangle" />
-    </div>
-  )
-}
